fe304650e2fec22566177d931fdb7ec8514da6a2
Streakman <46490210+MatthewLKomar@users.noreply.github.com>
2021-10-31 23:03:31 -0400

    removed github additions

--- Source/HeavenVR2/SimplifiedVRChar.h
+++ Source/HeavenVR2/SimplifiedVRChar.h
@@ -47,19 +47,12 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		class UCameraComponent* Camera = nullptr;
 
-<<<<<<< Updated upstream
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		class UMotionControllerComponent* LeftController;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		class UMotionControllerComponent* RightController;
-=======
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		AHandComponent* LeftController;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		AHandComponent* RightController;
->>>>>>> Stashed changes
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		class UGrabber* LeftGrabber;
